f110c0f86e00e47e6c5eb801a2b75288be5c60d1
merilinp <merilinpriidik99@gmail.com>
2021-11-06 16:24:33 +0200

    update

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ $(document).scroll(function () {
 
 //BACK TO TOP BUTTON
 
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
