f108e0599a102d6d47cec79103b7a913be40a87d
merilinp <merilinpriidik99@gmail.com>
2021-11-08 11:32:27 +0200

    update 08.11

--- backup/script.js
+++
@@ -1,8 +0,0 @@
-
-//NAVBAR SCROLLED
-
-$(document).scroll(function () {
-  var $hero = $(".hero-image");
-  var $nav = $(".fixed-top");
-  $nav.toggleClass('scrolled', $(this).scrollTop() > $hero.height() - 80);
-});
--- js/script.js
+++ js/script.js
@@ -9,17 +9,4 @@ $(document).scroll(function () {
 
 //BACK TO TOP BUTTON
 
-var btn = $('#button');
 
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
