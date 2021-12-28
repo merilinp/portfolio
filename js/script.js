7bfb2f38c8f599cd708d44ee70a191537f9465bb
merilinp <merilinpriidik99@gmail.com>
2021-12-28 13:16:09 +0200

    portfolio-2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 
-//NAVBAR SCROLLED
+//NAVBAR
 
 $(document).scroll(function () {
   var hero = $(".hero-image");
-  var nav = $(".fixed-top");
-  nav.toggleClass('scrolled', $(this).scrollTop() > hero.height() - 80);
+  var nav = $(".navbar");
+  nav.toggleClass('fixed-top', $(this).scrollTop() > hero.height() - 80);
 });
+
+// $(function () {
+//   /* $(".navbar-fixed-top").css({"top":$("#hero").height()});
+
+//       $(window).resize(function (e) {
+//       $(".navbar-fixed-top").css({"top":$("#hero").height()});
+//       }); */
+
+//   $(document).on('scroll', function () {
+//     console.log('scroll top : ' + $(window).scrollTop());
+//     if ($(window).scrollTop() >= $("#hero").height()) {
+//       $(".nav").addClass("navbar-fixed-top");
+//     }
+
+//     if ($(window).scrollTop() < $("#hero").height()) {
+//       $(".nav").removeClass("navbar-fixed-top");
+//     }
+//   });
+// });
 
 //BACK TO TOP BUTTON
 
